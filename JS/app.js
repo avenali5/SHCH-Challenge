eda10418c053075bb47abdfb677f233414f5d4c8
Alejo <git@github.com>
2021-03-07 15:58:39 -0300

    first commit

---
+++ JS/app.js
@@ -0,0 +1,27 @@
+let heart = document.querySelector('.heart-img')
+
+
+//1st step
+setTimeout(function(){
+    heart.src = 'assets/icons/heart-blue.svg'
+}, 1300)
+setTimeout(function(){
+    heart.src = 'assets/icons/heart.svg'
+}, 1500)
+
+//2nd step
+setTimeout(function(){
+   heart.src = 'assets/icons/heart-blue.svg'
+}, 2400)
+ setTimeout(function(){
+    heart.src = 'assets/icons/heart.svg'
+}, 2600)
+
+//3rd step
+setTimeout(function(){
+    heart.src = 'assets/icons/heart-blue.svg'
+}, 2700)
+setTimeout(function(){
+    heart.src = 'assets/icons/heart.svg'
+}, 2900)
+
